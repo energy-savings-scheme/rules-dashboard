d25a854a4ada4834a3b975d2d06196012bb6289d
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-02-22 15:11:25 +1100

    Chore // Create axios 'api_base' module - this is a handy convenience class, helpful for logging and debugging

---
+++ src/services/api_base.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  withCredentials: false
+});
+
+const ApiBase = function (options) {
+  console.log("Base url: ", process.env.REACT_APP_API_BASE_URL)
+  const onSuccess = function (response) {
+    return response.data;
+  };
+
+  const onError = function (error) {
+    console.error('Request Failed:', error.config);
+
+    if (error.response) {
+      // Request was made but server responded with something
+      // other than 2xx
+      console.error('Status:', error.response.status);
+      console.error('Data:', error.response.data);
+      console.error('Headers:', error.response.headers);
+    } else {
+      // Something else happened while setting up the request
+      // triggered the error
+      console.error('Error Message:', error.message);
+    }
+
+    return Promise.reject(error.response || error.message);
+  };
+
+  return client(options).then(onSuccess).catch(onError);
+};
+
+export default ApiBase;
