fc5d83be2a7d0249f2a08a763c8c514852135152
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-02-23 21:54:30 +1100

    Create <SearchBar /> component with navigation

---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function SearchBar(props) {
+  const { variables } = props;
+  const history = useHistory();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <div className="nsw-col nsw-col-md-6 nsw-offset-md-3">
+      <div className="nsw-form-search">
+        <label for="hero-search-input" className="sr-only">
+          Search a term below to find more information on related methods and requirements:
+        </label>
+        <input
+          className="nsw-form-search__input"
+          list="variables"
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <datalist id="variables">
+          {Object.keys(variables).map((variable) => (
+            <option value={variable} />
+          ))}
+        </datalist>
+        <button
+          className="nsw-form-search__submit"
+          onClick={(e) => {
+            history.push(`/variables/${searchTerm}`);
+          }}
+          aria-label="search"
+        >
+          <svg className="nsw-icon" focusable="false" aria-hidden="true">
+            <use xlinkHref="#search"></use>
+          </svg>
+          <span className="sr-only">Search</span>
+        </button>
+      </div>
+    </div>
+  );
+}
