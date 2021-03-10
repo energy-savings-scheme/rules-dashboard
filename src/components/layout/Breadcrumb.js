d790fbd32bb5795793d18fbfe84bbc0748acd826
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-03-10 15:45:42 +1100

    Fix // Breadcrumb formatting

--- src/components/layout/Breadcrumb.js
+++ src/components/layout/Breadcrumb.js
@@ -7,44 +7,85 @@ export default function Breadcrumb() {
 
   let path = {};
 
-  if (params.schedule_name) {
-    path = { breadcrumb_category: 'Schedules', breadcrumb_name: params.schedule_name };
-  }
-  if (params.variable_name) {
-    path = { breadcrumb_category: 'Variables', breadcrumb_name: params.variable_name };
+  if (params.schedule_name && !params.activity_sublabel) {
+    return (
+      <div className="nsw-container" style={{ marginBottom: 20 }}>
+        <nav aria-label="Breadcrumb" className="nsw-breadcrumb">
+          <ol className="nsw-breadcrumb__list">
+            <li className="nsw-breadcrumb__item">
+              <NavLink to="/" className="nsw-breadcrumb__link">
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nsw-breadcrumb__item">
+              <NavLink
+                to={`/variables/${params.schedule_name}`}
+                className="nsw-breadcrumb__link nsw-breadcrumb--current"
+              >
+                Schedule: {params.schedule_name}
+              </NavLink>
+            </li>
+          </ol>
+        </nav>
+      </div>
+    );
   }
 
-  return (
-    <div className="nsw-container" style={{ marginBottom: 20 }}>
-      <nav aria-label="Breadcrumb" className="nsw-breadcrumb">
-        <ol className="nsw-breadcrumb__list">
-          <li className="nsw-breadcrumb__item">
-            <NavLink
-              to="/"
-              className={
-                location.pathname === '/'
-                  ? 'nsw-breadcrumb__link nsw-breadcrumb--current'
-                  : 'nsw-breadcrumb__link'
-              }
-            >
-              Home
-            </NavLink>
-          </li>
-          {path.breadcrumb_name && (
-            <Fragment>
-              <li className="nsw-breadcrumb__item">{path.breadcrumb_category}</li>
-              <li className="nsw-breadcrumb__item">
-                <NavLink
-                  to={`/variables/${path.breadcrumb_name}`}
-                  className="nsw-breadcrumb__link nsw-breadcrumb--current"
-                >
-                  {path.breadcrumb_name}
-                </NavLink>
-              </li>
-            </Fragment>
-          )}
-        </ol>
-      </nav>
-    </div>
-  );
+  if (params.activity_sublabel) {
+    return (
+      <div className="nsw-container" style={{ marginBottom: 20 }}>
+        <nav aria-label="Breadcrumb" className="nsw-breadcrumb">
+          <ol className="nsw-breadcrumb__list">
+            <li className="nsw-breadcrumb__item">
+              <NavLink to="/" className="nsw-breadcrumb__link">
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nsw-breadcrumb__item">
+              <NavLink to={`/schedules/${params.schedule_name}`} className="nsw-breadcrumb__link">
+                Schedule: {params.schedule_name}
+              </NavLink>
+            </li>
+
+            <li className="nsw-breadcrumb__item">
+              <NavLink
+                to={`/schedules/${params.schedule_name}/${params.activity_sublabel}`}
+                className="nsw-breadcrumb__link nsw-breadcrumb--current"
+              >
+                Activity: {params.activity_sublabel}
+              </NavLink>
+            </li>
+          </ol>
+        </nav>
+      </div>
+    );
+  }
+  if (params.variable_name) {
+    return (
+      <div className="nsw-container" style={{ marginBottom: 20 }}>
+        <nav aria-label="Breadcrumb" className="nsw-breadcrumb">
+          <ol className="nsw-breadcrumb__list">
+            <li className="nsw-breadcrumb__item">
+              <NavLink to="/" className="nsw-breadcrumb__link">
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nsw-breadcrumb__item">
+              <NavLink
+                to={`/variables/${params.variable_name}`}
+                className="nsw-breadcrumb__link nsw-breadcrumb--current"
+              >
+                Variable: {params.variable_name}
+              </NavLink>
+            </li>
+          </ol>
+        </nav>
+      </div>
+    );
+  }
+
+  return null;
 }
