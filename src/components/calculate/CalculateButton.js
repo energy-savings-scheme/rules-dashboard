08da57a092be8cf836e48196b7c73332906c4084
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-03-30 16:57:51 +1100

    Render result after calculation

--- src/components/calculate/CalculateButton.js
+++ src/components/calculate/CalculateButton.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import OpenFiscaApi from 'services/openfisca_api';
 
 import Button from 'nsw-ds-react/button/button';
+import { Notification } from 'nsw-ds-react/notification/notification';
 
 export default function CalculateButton(props) {
   const { variable, entities } = props;
   var { formValues } = props;
+
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(false);
 
   const validateDataType = (item) => {
     // OpenFisca requires payload data to be correctly typed.
@@ -20,6 +24,8 @@ export default function CalculateButton(props) {
   };
 
   const handleCalculate = (e) => {
+    setError(false);
+
     const entity = entities.find((item) => item.name === variable.entity);
 
     // Set implementation date from DateInput component - and pop item from formValues
@@ -41,16 +47,51 @@ export default function CalculateButton(props) {
 
     OpenFiscaApi.postCalculate(payload)
       .then((res) => {
-        console.log(res);
+        var result = res.data[entity.plural][`${entity.name}_1`][variable.name][date];
+        setResult(result);
       })
       .catch((err) => {
+        setResult(null);
+        setError(true);
         console.log(err);
       });
   };
 
   return (
-    <Button as="primary" onClick={handleCalculate}>
-      Calculate
-    </Button>
+    <Fragment>
+      {result !== null && (
+        <div className="nsw-row">
+          <div className="nsw-col">
+            <div className="nsw-content-block">
+              <div className="nsw-content-block__content">
+                <h4 className="nsw-content-block__title" style={{ textAlign: 'center' }}>
+                  Based on the information provided, your{' '}
+                  <span style={{ fontWeight: 600, textDecoration: 'underline' }}>
+                    {variable.metadata && variable.metadata.alias
+                      ? variable.metadata.alias
+                      : variable.name}
+                  </span>{' '}
+                  are
+                </h4>
+                <h1 style={{ textAlign: 'center', paddingTop: 10 }}>{result}</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {error && (
+        <Notification as="error" title="Sorry! An error has occurred.">
+          <p>
+            An error occurred during calculation. Try choosing a more recent Date and re-running the
+            calculation
+          </p>
+        </Notification>
+      )}
+
+      <Button as="primary" onClick={handleCalculate}>
+        {result ? 'Calculate again' : 'Calculate'}
+      </Button>
+    </Fragment>
   );
 }
