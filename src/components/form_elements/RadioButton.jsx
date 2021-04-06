a4051b9c3c195eb9702746c4f1cedf936ff0ee70
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-04-06 15:24:11 +1000

    Fix // make radio select onChange handler work

--- src/components/form_elements/RadioButton.jsx
+++ src/components/form_elements/RadioButton.jsx
@@ -5,27 +5,20 @@ export default function RadioInput(props) {
   const { formItem, setItemValue } = props;
 
   const possibleValues = [
-    { value: false, text: 'false', checked: 'true' },
-    { value: true, text: 'true', checked: 'false' },
+    { value: false, text: 'False' },
+    { value: true, text: 'True' },
   ];
-  // for (const [key, value] of Object.entries(formItem.possible_values)) {
-  //   possibleValues.push({ text: value, value: key });
-  // }
-
-  console.log(formItem);
 
   return (
     <FormGroupRadio
-      // helper={
-      //   formItem.metadata && formItem.metadata.alias ? formItem.metadata.alias : formItem.name
-      // } // (secondary label)
+      helper={
+        formItem.metadata && formItem.metadata.alias ? formItem.metadata.alias : formItem.name
+      } // (secondary label)
       label={formItem.description} // primary label
       errorText="Invalid value!" // error text if invalid
       status={formItem.invalid && 'invalid'} // if `true` renders invalid formatting
       options={possibleValues}
-      // onClick={() => {
-      //   console.log('clicked');
-      // }}
+      onChange={setItemValue}
     ></FormGroupRadio>
   );
 }
