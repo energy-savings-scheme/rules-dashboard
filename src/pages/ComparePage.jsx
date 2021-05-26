b4733160609e45ed6b0d2edf40e8220992ccacd7
YuguangIpsen <yuguang.fan@gmail.com>
2021-05-26 11:52:08 +1000

    Update: Compare Variable page with RegRef

--- src/pages/ComparePage.jsx
+++ src/pages/ComparePage.jsx
@@ -11,6 +11,8 @@ export default function ComparePage(props) {
   const [stepNumber, setStepNumber] = useState(1);
   const [variable1, setVariable1] = useState({});
   const [variable2, setVariable2] = useState({});
+  const [var1_ref, setVar1Ref] = useState({});
+  const [var2_ref, setVar2Ref] = useState({});
 
   const populateDropDown = (newOption) => {
     setDropdownOptions((prev) => {
@@ -21,6 +23,34 @@ export default function ComparePage(props) {
   useEffect(() => {
     variables.forEach((item) => populateDropDown({ text: item.metadata.alias, value: item.name }));
   }, [variables]);
+
+  const unpackRegRef = (someVariable) => {
+    const metadata = someVariable.metadata;
+    if (metadata !== null) {
+      var metadataKeys = Object.keys(metadata);
+      if (metadataKeys.includes('regulation_reference')) {
+        var ref = metadata.regulation_reference;
+        var reg_ref = { version_name: ref['version'], version_code: ref['identifier'] };
+
+        while (Object.keys(ref).includes('part')) {
+          var ref = ref['part'];
+          if (ref['part_type'] === 'SubMethod') {
+            reg_ref['subMethod'] = ref['title'];
+          }
+
+          if (ref['part_type'] === 'Activity Definition') {
+            reg_ref['activity'] = ref['title'];
+          }
+        }
+
+        return reg_ref;
+      } else {
+        return {};
+      }
+    } else {
+      return {};
+    }
+  };
 
   if (!variable1) return null;
   if (!variable2) return null;
@@ -48,7 +78,9 @@ export default function ComparePage(props) {
                       options={dropdownOptions}
                       value={variable1.name}
                       onChange={(e) => {
-                        setVariable1(variables.find((item) => item.name === e.target.value));
+                        const var1 = variables.find((item) => item.name === e.target.value);
+                        setVariable1(var1);
+                        setVar1Ref(unpackRegRef(var1));
                       }}
                     ></FormGroupSelect>
                     <FormGroupSelect
@@ -57,7 +89,9 @@ export default function ComparePage(props) {
                       options={dropdownOptions}
                       value={variable2.name}
                       onChange={(e) => {
-                        setVariable2(variables.find((item) => item.name === e.target.value));
+                        const var2 = variables.find((item) => item.name === e.target.value);
+                        setVariable2(var2);
+                        setVar2Ref(unpackRegRef(var2));
                       }}
                     ></FormGroupSelect>
                   </div>
@@ -88,8 +122,8 @@ export default function ComparePage(props) {
                 <div className="nsw-content-block">
                   <div className="nsw-content-block__content">
                     <div className="nsw-table-responsive">
+                      <p>Here are the details of these two variables.</p>
                       <table className="nsw-table nsw-table--caption-top nsw-table--bordered nsw-table--striped ">
-                        <caption>Here are the details of these two variables.</caption>
                         <thead>
                           <tr>
                             <th width="20%"></th>
@@ -119,17 +153,25 @@ export default function ComparePage(props) {
                           <tr>
                             <td>
                               {' '}
-                              <strong>Activity Name</strong>
+                              <strong>Scheme</strong>
                             </td>
-                            <td>{variable1.metadata.minorCat}</td>
-                            <td>{variable2.metadata.minorCat}</td>
+                            <td>{var1_ref.version_code}</td>
+                            <td>{var2_ref.version_code}</td>
                           </tr>
                           <tr>
                             <td>
-                              <strong>Activity Group</strong>
+                              {' '}
+                              <strong>Sub-Method</strong>
                             </td>
-                            <td>{variable1.metadata.majorCat}</td>
-                            <td>{variable2.metadata.majorCat}</td>
+                            <td>{var1_ref.subMethod}</td>
+                            <td>{var2_ref.subMethod}</td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <strong>Activity Name</strong>
+                            </td>
+                            <td>{var1_ref.activity}</td>
+                            <td>{var2_ref.activity}</td>
                           </tr>
                           <tr>
                             <td>
