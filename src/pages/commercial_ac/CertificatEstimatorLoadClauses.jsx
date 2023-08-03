1e6870e3ea3ff1429238b6516195f8245349262e
alisha17 <anejaalisha37@gmail.com>
2023-08-03 10:02:41 +1000

    change estimator ui flow

--- src/pages/commercial_ac/CertificatEstimatorLoadClauses.jsx
+++ src/pages/commercial_ac/CertificatEstimatorLoadClauses.jsx
@@ -313,10 +313,10 @@ export default function CertificateEstimatorLoadClauses(props) {
                   as="dark-outline-solid"
                   onClick={(e) => {
                     setFlow('backward');
-                    setStepNumber(stepNumber - 1);
+                    setStepNumber(stepNumber - 2);
                   }}
                 >
-                  Back
+                  Calculate certificates again
                 </Button>
               </div>
 
